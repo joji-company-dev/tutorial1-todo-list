2f0b35e6ed231ae7373f861115930810f9abd360
dohee12 <155977634+dohee12@users.noreply.github.com>
2024-12-18 14:57:30 +0900

    함수사용

---
+++ src/app/todo-list/project/page.js
@@ -0,0 +1,17 @@
+'use client'
+
+export default function ProjectPage (){
+
+return (
+    <h1>Hello world</h1>
+)
+};
+
+
+// javascript에서 함수를 선언하는 방법
+
+// 함수 표현식을 이용한 방법
+// const a = () => {};
+
+// 함수 선언문을 이용한 방법
+function a (){}
