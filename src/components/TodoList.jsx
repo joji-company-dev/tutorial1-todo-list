3515f857eb5a0aca7a5aaf68dde93217b368f7c3
Alang Kim <rlaangh77@gmail.com>
2024-12-18 14:07:26 +0900

    feat: 할일 목록을 추가할 수 있는 TodoList 컴포넌트 작성

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+/**
+ * @typedef {Object} Todo
+ * @property {string} id
+ * @property {string} title
+ * @property {boolean} isDone
+ */
+
+export function TodoList() {
+  /**
+   * @type {Todo[]}
+   */
+  const [todoList, setTodoList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    if (!title) return;
+    setTodoList([...todoList, { id: Date.now(), title, isDone: false }]);
+    e.target.title.value = "";
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex gap-2">
+        <input name="title" type="text" className="border text-black" />
+        <button className="border p-2">추가</button>
+      </form>
+
+      <ul className="flex flex-col gap-2">
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
