0b2efb1c2382d601bb7cf97dbaa68a64205bfdda
MuteAim <ji_hyunsoo@naver.com>
2024-12-18 17:22:46 +0900

    feat: 할일체크기능 추가

--- src/app/todo-list/components/TodoItem.js
+++ src/app/todo-list/components/TodoItem.js
@@ -1,10 +1,24 @@
+"use client"
+import { useState } from "react";
+
 export default function TodoItem({ 
     id, 
     text, 
     onDelete 
 }) {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChecked = () =>{
+        setIsChecked(!isChecked)
+    }
     return (
       <li className="flex justify-between items-center border-b p-2">
+        <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleChecked}
+        />
         <span>{text}</span>
         <button
           onClick={() => onDelete(id)}
