4ec25d4d342b1e519301eabce909832ef40e8278
MuteAim <ji_hyunsoo@naver.com>
2024-12-22 17:12:48 +0900

    feat: 로컬스토리지 데이터 삭제구현
    새로고침 및 재접속해도 정상작동

--- src/app/todo-list/project/page.js
+++ src/app/todo-list/project/page.js
@@ -3,42 +3,30 @@
 import { useEffect, useState } from "react";
 import TodoItem from "../components/TodoItem";
 
-export const useStateWithLocalStorage = (key, defaultValue) => {
-  const [state, setState] = useState(defaultValue);
-
-  const setStateWithPersistence = (state) => {
-    localStorage.setItem(key, JSON.stringify(state));
-    setState(state);
-  }
-
-  useEffect(() => {
-     const loadedStateJson = localStorage.getItem(key);
-     if(storageState){
-        setState(JSON.parse(loadedStateJson));
-     }
-  },[])
-
-
-  return [state, setStateWithPersistence]
-}
-
-
 export default function TodoList() {
   const [todoList, setTodoList] = useState([]);
   const [input, setInput] = useState("");
 
+  
+  // 로컬스토리지에서 데이터를 가져오는 useEffect
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      try {
+        setTodoList(JSON.parse(todos));
+      } catch (error) {
+        console.error("Error parsing todos from localStorage:", error);
+        setTodoList([]); // 에러가 발생할 경우 빈 배열로 초기화
+      }
+    } else {
+      setTodoList([]); // null일 경우 빈 배열로 초기화
+    }
+  }, []);
+  
   const setTodoListWithPersistence = (todoList) => {
     localStorage.setItem('todos', JSON.stringify(todoList));
     setTodoList(todoList);
   }
-
-   // 로컬스토리지에서 데이터를 가져오는 useEffect
-   useEffect(()=>{
-    const todos = localStorage.getItem('todos');
-    if(todos){
-        setTodoList(JSON.parse(todos));
-    }
-   },[]);
    //useEffect(()=> {...},[] )
    //여기서 []는 컴포넌트가 마운트될때 무한루프를 방지한다. 
    //처음에 오류가 난이유는 의존성배열[]를 넣지않았기에 무한루프가 돌아 오류가난것
@@ -59,7 +47,8 @@ export default function TodoList() {
   };
 
   const handleDelete = (id) => {
-    setTodoList(todoList.filter((todo) => todo.id !== id));
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoListWithPersistence(updatedTodoList);
   };
 
 //   const handleCheck =() =>{
