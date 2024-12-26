389813ca3cf77c42a3bdc0dde2c661af73df5cfc
MuteAim <ji_hyunsoo@naver.com>
2024-12-26 23:40:15 +0900

    feat: 이즐리 게시글 페이지 추가
    경로: easily-posts/project/page.js

---
+++ src/app/easily-posts/project/page.js
@@ -0,0 +1,7 @@
+export default function EasilyPosts() {
+  return (
+    <div className="max-w-md mx-auto mt-10 p-5 border rounded shadow">
+      <h1 className="text-2xl font-bold text-center mb-4">EasilyPosts</h1>
+    </div>
+  );
+}
